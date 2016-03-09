412addfb8fe4e601f4425dceeb0c62dc587e129c
denproger <denproger@mail.ru>
2016-03-09 15:05:23 +0300

    Add files for lesson 1.

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
