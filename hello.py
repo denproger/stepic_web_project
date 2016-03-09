e0e94da18e2e8881fec43e3186ffa9733b21e22b
denproger <denproger@mail.ru>
2016-03-09 18:34:59 +0300

    Исправления

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
 def application(environ, start_response):
-    status = "200 OK"
-    headers = [ ('Content-Type', 'text/plant') ]
-    body=environ['QUERY_STRPING'].replace('&', '\n')
-    start_response(status, headers )
-    return [ body ]
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = environ['QUERY_STRING'].replace('&', '\n')
+    start_response(status, headers)
+    return [body]
