3761a7b289811a1001f2f3eed91822324de9e250
denproger <denproger@mail.ru>
2016-03-09 15:18:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo rm /etc/nginx/sites-enabled/default
 sudo /bin/ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
