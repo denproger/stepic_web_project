d663c584d0d35646235923fcfcdfb563f2b88577
denproger <denproger@mail.ru>
2016-03-09 18:58:07 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-#sudo /bin/ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 web.hello:application -D
+sudo rm /etc/nginx/sites-enabled/default
+sudo /bin/ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+#cd /home/box/web
+#gunicorn -b 0.0.0.0:8080 web.hello:application -D
 sudo /bin/ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
