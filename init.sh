1718bdbc6876d87e0d4c119ea06310d5b0ffdcf5
denproger <denproger@mail.ru>
2016-03-11 05:46:11 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 sudo /bin/ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo /bin/ln -sf /home/box/web/ask/ask.gunicorn.conf  /etc/gunicorn.d/ask
+sudo /bin/ln -sf /home/box/web/etc/ask.gunicorn.conf  /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
 #cd /home/box/web
