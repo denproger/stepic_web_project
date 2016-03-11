1961542e6b74e9a296a3cb54fdc8f280d7a14241
denproger <denproger@mail.ru>
2016-03-11 05:25:09 +0300

    Добалено задание 2_1

--- init.sh
+++ init.sh
@@ -2,9 +2,12 @@
 if [ -f /etc/nginx/sites-enabled/default ]; then 
     sudo rm /etc/nginx/sites-enabled/default
 fi
-#sudo /bin/ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-cd /home/box/web
-gunicorn -b 0.0.0.0:8080 web.hello:application -D
+sudo /bin/ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo /bin/ln -sf /home/box/web/ask/ask.gunicorn.conf  /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
+
+#cd /home/box/web
+#gunicorn -b 0.0.0.0:8080 web.hello:application -D
+
 sudo /bin/ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
